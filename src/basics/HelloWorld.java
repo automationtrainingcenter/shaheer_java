cdde615fdaf115e803c00b5ff1d8fd75f3d1223e
automation <atcsurya@gmail.com>
2019-07-08 12:17:40 +0530

    notes folder added with installation and cloning instructions

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("hello world....");
 		System.out.println("Welcome to sunshine");
 		System.out.println("Java Course by Surya");
+		System.out.println("test git");
 	}
 }
